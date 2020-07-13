5a386677a9533333e681e1553823a7c8eba3e598
bobbybrooksatx <brooks@mindsetatx.com>
2020-07-13 17:35:18 -0500

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,35 @@ module.exports = {
     description: "Let's work together, here's my resume.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: ["@reflexjs/gatsby-theme-base"],
+  plugins: [
+    "@reflexjs/gatsby-theme-base",
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-M2654PS",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "BBWork-Data-Layer",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "Page View",
+      },
+    },
+  ],
 }
